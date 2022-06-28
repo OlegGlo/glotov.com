6521bcf767de9bdf2821dc6fc68d0c7d5ec07cf2
OlegGlo <olegglotov99@gmail.com>
2022-06-28 03:51:31 -0400

    quick fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import Spline from '@splinetool/react-spline';
-import { AboutSection, aboutSection } from './sections/AboutSection'
+import { AboutSection } from './sections/AboutSection'
 
 function App() {
   return (
